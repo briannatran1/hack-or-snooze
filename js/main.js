4c02f70ee79f777dac8aacbc67c2c52b8e0abbd7
Brianna Tran <briannatran45@gmail.com>
2023-08-18 16:20:10 -0700

    added global var

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ const $navLogOut = $("#nav-logout");
 const $storyForm = $("#form-submit");
 
 const $navSubmit = $("#nav-submit");
+const $navFavorites = $("#favorites-tab");
 
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
